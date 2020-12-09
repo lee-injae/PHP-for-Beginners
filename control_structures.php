6710e18c007a0ef232e76f027a64fd67ef5f5796
lee-injae <ilee.injae@gmail.com>
2020-12-09 11:43:50 -0500

    control_structures

---
+++ control_structures.php
@@ -0,0 +1,24 @@
+<?php 
+
+$articles = [];
+
+var_dump(empty($articles));
+
+if (empty($articles)){
+    echo "the array is empty";
+} else {
+    echo "array is not empty";
+}
+
+var_dump( 4 == "4");
+
+$age = 22;
+
+if ($age < 21) {
+    echo "condition true";
+} else {
+    echo "condition false";
+}
+
+
+
